d2cca5e4721a50d39984418bf6861976ec3eca18
dinesh <dineshreddy.mw@gmail.com>
2020-07-05 15:06:52 -0400

    undo changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -18,7 +18,7 @@ export default function App() : JSX.Element {
   });
 
   if(loadError) return <div>Error loading maps</div>
-  if(!isLoaded) return   "Loading Maps"
+  if(!isLoaded) return  <div> Loading Maps </div>
  
 
   const handlingView = (event: FormEvent) => {
